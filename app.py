7ea0bc7d6906527e711067bab8236f751352a8c7
viccon23 <victor.contreras23@aol.com>
2023-05-06 22:18:21 -0500

    Better Overlay, more progress needed on Menu

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ def greet(name):
 @app.route("/")
 
 def homeInfo():
-    title = "My Personal Site"
     details = readDetails('static/Summarytitle.txt')
     name = "Victor Contreras"
-    ## aboutMe = ["Hi, my name is Victor and I'm a Computer Science student at UTRGV.", "I'm passionate about Coding and learning about Technology, whether it's Frontend, Backend, or Full Stack."]
     return render_template("base.html", name=name, aboutMe=details)
 
 
@@ -29,27 +27,14 @@ def writeToFile(filename, message):
     with open(filename, 'a') as f:
         f.write(message)
 
-@app.route("/Goals")
-def goals():
-    my_goals = ['Learn more languages like Rust, mySQL, Java and Frameworks like Node.js', 'Try to obtain an internship', 'Practice Coding', 'Network with professors and classmates']
-    return render_template('Goals.html', Goals=my_goals)
-
-@app.route("/Projects")
+@app.route("/Wordle")
 def projects():
-    return "<h1> None Yet! Maybe this one could be the first... </h1>"
+    return render_template("wordle.html")
 
-@app.route('/form', methods=['GET', 'POST'])
-def formDemo():
-    name = None
-    if request.method == 'POST':
-        if request.form['name']:
-           name = request.form['name']
-        #if request.form['message']:
-            #writeToFile('/static/comments.txt', request.form['message'])
-    return render_template('form.html', name=name)
-
-
-
+@app.route("/DiscordMessageBot")
+def project():
+    return render_template("MessageBot.html")
+    
 
 ## When running this file directly...
 if __name__ == "__main__":
